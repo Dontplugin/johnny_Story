4c6b6bf8ff59bba2ddafcc3f821ac2e733bcd8e1
韩宇哲 <hanyuzhe@souche.com>
2020-04-06 23:54:48 +0800

    feat: 增加状态机的使用示例 :zap:

---
+++ src/main/java/top/sinfulxx/stateMachine/Application.java
@@ -0,0 +1,31 @@
+package top.sinfulxx.stateMachine;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.statemachine.StateMachine;
+
+/**
+ * @author hanyuzhe
+ * @date 2020/4/6 11:40 下午
+ * @since 1.0
+ */
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Autowired
+    private StateMachine<States, Events> stateMachine;
+
+    @Override
+    public void run(String... args) throws Exception {
+        stateMachine.start();
+        stateMachine.sendEvent(Events.PAY);
+        stateMachine.sendEvent(Events.RECEIVE);
+    }
+
+}
---
+++ src/main/java/top/sinfulxx/stateMachine/Events.java
@@ -0,0 +1,11 @@
+package top.sinfulxx.stateMachine;
+
+/**
+ * @author hanyuzhe
+ * @date 2020/4/6 11:40 下午
+ * @since 1.0
+ */
+public enum Events {
+    PAY,        // 支付
+    RECEIVE     // 收货
+}
